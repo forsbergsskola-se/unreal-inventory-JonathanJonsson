a63e9364d58ecc0a0f0a6572d34ac6757e5332da
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-09-07 14:39:22 +0200

    Added first C++ code

---
+++ Source/UEIntroAssign/Public/TestActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestActor.generated.h"
+
+UCLASS()
+class UEINTROASSIGN_API ATestActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void DrawDiamond(FVector Location, float Radius, int Segments,FColor Color );
+	// Sets default values for this actor's properties
+	ATestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
