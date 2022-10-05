73837e35e13f5fc9dde112983bcdca6aff0d886f
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-10-05 20:42:42 +0200

    Made disco slime. What's not to love?!

---
+++ Source/UEIntroAssign/Public/ActorWithMaterialManipulator.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "UObject/Object.h"
+#include "ActorWithMaterialManipulator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEINTROASSIGN_API AActorWithMaterialManipulator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="GP_JJ")
+	FLinearColor GetRandomColor();
+};
